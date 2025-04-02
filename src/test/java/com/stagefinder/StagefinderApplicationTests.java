1e182a0ecb0f4c03022ee2dfffa4e1ecb697fa2e
Ahmed Debbab <ahmed.debbab.dev@gmail.com>
2025-04-02 23:55:27 +0000

    creation du projet avec les dependences (s.web, s.data jpa, lombok, devtools, postgresSQL driver, s.security) + la configuration PostgresSql, Hibernate/jpa ainsi que liquibase

---
+++ src/test/java/com/stagefinder/StagefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stagefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagefinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
