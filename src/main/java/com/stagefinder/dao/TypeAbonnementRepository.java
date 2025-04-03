23b825c9253b1d4cc9b26eb18ae03b1a5f7f44ce
Ahmed Debbab <ahmed.debbab.dev@gmail.com>
2025-04-03 09:28:55 +0000

    l ajout des repositories

--- src/main/java/com/stagefinder/dao/TypeAbonnementRepository.java
+++ src/main/java/com/stagefinder/dao/TypeAbonnementRepository.java
@@ -3,5 +3,5 @@ package com.stagefinder.dao;
 import com.stagefinder.entities.TypeAbonnement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TypeAbonnementRepository extends JpaRepository<TypeAbonnement, Integer> {
+public interface TypeAbonnementRepository extends JpaRepository<TypeAbonnement, Long> {
 }
